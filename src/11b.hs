ee08eb95e559571126eecddb140790d232a76fe7
mingmingrr <mingmingrr@gmail.com>
2019-04-18 11:04:50 -0400

    Day 11 part 1 done

---
+++ src/11b.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import Data.List
+import Data.Function
+import Control.Lens
+import Control.Monad
+import Numeric.LinearAlgebra
+
+grid :: Integer -> Matrix Double
+grid n = build (300, 300) (builder `on` (+1) . round)
+  where builder x y = let id = x + 10
+                       in fromInteger . subtract 5
+                        $ (id * y + n) * id `mod` 1000 `div` 100
+
+main = getContents >>= print
+                     . (_2 %~ snd)
+                     . maximumBy (compare `on` snd)
+                     . zip ([1..300] :: [Int])
+                     . map (liftM2 (,) maxElement maxIndex)
+                     . zipWith conv2 (map (konst 1 . join (,)) [1..300])
+                     . repeat . grid . read
