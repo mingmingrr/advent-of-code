98f8748479087102eedc83b706a3d2c8378a0f83
mingmingrr <mingmingrr@gmail.com>
2019-04-19 23:17:46 -0400

    11b: indent change

--- src/11b.hs
+++ src/11b.hs
@@ -12,11 +12,12 @@ grid n = build (300, 300) (builder `on` (+1) . round)
                        in fromInteger . subtract 5
                         $ (id * y + n) * id `mod` 1000 `div` 100
 
-main = getContents >>= putStrLn
-                     . (\(z, (x, y)) -> show (x+1) ++ "," ++ show (y+1) ++ "," ++ show z)
-                     . (_2 %~ snd)
-                     . maximumBy (compare `on` snd)
-                     . zip ([1..300] :: [Int])
-                     . map (liftM2 (,) maxElement maxIndex)
-                     . zipWith corr2 (map (konst 1 . join (,)) [1..300])
-                     . repeat . grid . read
+main = getContents
+   >>= putStrLn
+     . (\(z, (x, y)) -> show (x+1) ++ "," ++ show (y+1) ++ "," ++ show z)
+     . (_2 %~ snd)
+     . maximumBy (compare `on` snd)
+     . zip ([1..300] :: [Int])
+     . map (liftM2 (,) maxElement maxIndex)
+     . zipWith corr2 (map (konst 1 . join (,)) [1..300])
+     . repeat . grid . read
