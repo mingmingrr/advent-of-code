2414afd9379c6eb6a706e185b8ec08ccccac1a1d
Shiming Deng <sdeng@highgarden.local>
2019-05-23 13:02:47 -0400

    2015.11: complete

---
+++ src/2015/11b.hs
@@ -0,0 +1,24 @@
+import Data.Char
+import Data.Either
+import Text.Parsec
+import Control.Monad
+
+valid = (isRight .) . flip parse "" $ do
+  notFollowedBy $ manyTill anyChar (oneOf "iol")
+  lookAhead . manyTill anyChar . try $ do
+    [a, b, c] <- count 3 anyChar
+    guard $ (a == pred b) && (b == pred c)
+  lookAhead . manyTill anyChar . try $ do
+    a <- anyChar >>= char
+    manyTill anyChar . try $
+      anyChar >>= char >>= guard . (/=a)
+
+next = reverse . iter . reverse
+  where iter [] = []
+        iter ('z':as) = 'a' : iter as
+        iter (a:as) = succ a : as
+
+main = interact $ until valid next . next
+                . until valid next . next
+                . head . lines
+
