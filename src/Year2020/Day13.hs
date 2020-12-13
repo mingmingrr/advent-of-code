e9c5853895bd24e9833ed4fcbe143c5bf5aeeb9b
mingmingrr <mingmingrr@gmail.com>
2020-12-13 13:53:06 -0500

    2020.13: small cleanup

--- src/Year2020/Day13.hs
+++ src/Year2020/Day13.hs
@@ -44,6 +44,7 @@ import qualified Text.Megaparsec.Char as Par
 import qualified Text.Megaparsec.Char.Lexer as Lex
 
 import qualified Data.SBV as SBV
+import qualified Data.SBV.Internals as SBV
 import Data.String.Here
 import Data.Default
 import Data.Word
@@ -121,22 +122,22 @@ invMod :: Integer -> Integer -> Integer
 invMod n m = case invertSomeMod (n `modulo` fromInteger m) of
   Just (SomeMod k) -> getVal k
 
-part1, part2, part2' :: Integer -> [(Integer, Integer)] -> IO ()
-part1 t = print . uncurry (*) . first (subtract t) . minimum
+part1, part2, part2' :: Integer -> [(Integer, Integer)] -> IO Integer
+part1 t = return . uncurry (*) . first (subtract t) . minimum
   . map ((\n -> (div (t - 1) n * n + n, n)) . snd)
-part2 t = print . snd . foldl func (1,1) where
+part2 t = return . snd . foldl func (1,1) where
   func (m,n) (i,v) = (lcm m v, n + m * mod ((-i-n) * invMod m v) v)
-part2' t xs = print <=< SBV.satWith SBV.yices $ do
-  n <- SBV.sInteger "n"
-  SBV.constrain $ n SBV..> 0
-  forM_ xs $ \(i, x) -> SBV.constrain $
-    snd (SBV.sQuotRem (n + fromInteger i) (fromInteger x)) SBV..== 0
+part2' t xs = fmap (fromJust . SBV.getModelValue "n")
+  . SBV.satWith SBV.yices $ do
+    n <- SBV.sInteger "n"
+    SBV.constrain $ n SBV..> 0
+    forM_ xs $ \(i, x) -> SBV.constrain $
+      SBV.sRem (n + fromInteger i) (fromInteger x) SBV..== 0
 
 main = do
   input <- readFile (replaceExtension __FILE__ ".in")
   let [time, nums] = lines input
-  part2' (read time) . catMaybes
-    . zipWith (\x y -> (x,)<$>y) [0..]
-    . map (readsMaybe readDec)
+  print <=< part2' (read time) . catMaybes
+    . zipWith (\x y -> (x,) <$> readsMaybe readDec y) [0..]
     $ splitOn "," nums
 
