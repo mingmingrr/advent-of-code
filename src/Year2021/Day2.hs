7c98a42570277a7887a59491890274fb28ecb073
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-02 11:27:10 -0500

    2021.2: use State

--- src/Year2021/Day2.hs
+++ src/Year2021/Day2.hs
@@ -1,20 +1,31 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Year2021.Day2 where
 
 import System.FilePath
+import Control.Monad.State
+import Control.Lens
 
-part1, part2 :: Char -> Int -> (Int, Int, Int) -> (Int, Int, Int)
-part1 'f' n (horiz, vert, _) = (horiz + n, vert, 0)
-part1 'd' n (horiz, vert, _) = (horiz, vert - n, 0)
-part1 'u' n (horiz, vert, _) = (horiz, vert + n, 0)
-part2 'f' n (horiz, vert, aim) = (horiz + n, vert + aim * n, aim)
-part2 'd' n (horiz, vert, aim) = (horiz, vert, aim - n)
-part2 'u' n (horiz, vert, aim) = (horiz, vert, aim + n)
+data Submarine = Submarine
+  { _horiz :: Int
+  , _vert :: Int
+  , _aim :: Int
+  } deriving (Eq, Show, Ord)
+makeLenses ''Submarine
+
+part1, part2 :: Int -> Char -> State Submarine ()
+part1 n 'f' = horiz += n
+part1 n 'd' = vert -= n
+part1 n 'u' = vert += n
+part2 n 'f' = horiz += n >> join (uses aim ((vert +=) . (* n)))
+part2 n 'd' = aim -= n
+part2 n 'u' = aim += n
 
 main = do
   readFile (replaceExtension __FILE__ ".in") >>= print
-    . (\(x, y, z) -> x * abs y)
-    . foldl (\xs [c:_, n] -> part2 c (read n) xs) (0, 0, 0)
-    . map words . lines
+    . ((*) <$> _horiz <*> abs . _vert) . flip execState (Submarine 0 0 0)
+    . mapM_ (\(words -> [c:_, n]) -> part2 (read n) c) . lines
 
