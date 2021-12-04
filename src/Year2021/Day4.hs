58d0dffc05ee9cf24b008081995a3638d95f0bfa
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-04 01:55:52 -0500

    2021.4: simplify

--- src/Year2021/Day4.hs
+++ src/Year2021/Day4.hs
@@ -1,19 +1,17 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Year2021.Day4 where
 
 import Util
 import Data.List
-import Data.List.Extra (minimumOn, maximumOn)
 import Data.List.Split
 import Control.Lens
 import System.FilePath
 
-part1, part2 :: Ord b => (a -> b) -> [a] -> a
-part1 = maximumOn
-part2 = minimumOn
+part1, part2 :: [a] -> a
+part1 = last
+part2 = head
 
 play :: [Int] -> [[(Int, Int)]] -> (Int, Int)
 play (x:xs) board = if any (all ((== 1) . fst)) ((<*>) (++) transpose board)
@@ -21,7 +19,7 @@ play (x:xs) board = if any (all ((== 1) . fst)) ((<*>) (++) transpose board)
   else play xs (set (mapped . mapped . filtered (views _2 (== x)) . _1) 1 board)
 
 main = readFile (replaceExtension __FILE__ ".in") >>= \xs ->
-  let (ns@(reverse -> ms) : bs) = map readOnlyNums (lines xs)
-   in print . uncurry ((*) . (ms !!)) . part2 fst
+  let (ns:bs) = map readOnlyNums (lines xs)
+   in print . uncurry ((*) . (reverse ns !!)) . part2 . sort
         . map (play ns . map (map (0,)) . tail) $ chunksOf 6 bs
 
