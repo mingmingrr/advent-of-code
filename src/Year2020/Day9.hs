a05647b14cfa4e090a18dde2d3873afe59a3950f
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-04 13:21:40 -0500

    2020.9: rewrite

--- src/Year2020/Day9.hs
+++ src/Year2020/Day9.hs
@@ -3,35 +3,18 @@
 
 module Year2020.Day9 where
 
-import Data.Foldable
-import Data.Semigroup
+import Util
 import Data.List
-
-import Control.Arrow
-import Control.Lens (_1, _2, _3)
-import qualified Control.Lens as Lens
-import Control.Lens.Operators
-
+import Control.Monad
 import System.FilePath
 
-import Data.Sequence (Seq((:<|), (:|>)))
-import qualified Data.Sequence as Seq
-
-takeTilFail :: [Int] -> [Int]
-takeTilFail = uncurry look . first Seq.fromList . splitAt 25 where
-  look xs@(_:<|xt) (y:ys) =
-    if or [a + b == y | a <- toList xs, b <- toList xs]
-      then y : look (xt:|>y) ys else [y]
-
 part1, part2 :: [Int] -> Int
-part1 = last
-part2 (Lens.unsnoc -> Just (xs, total)) = head
-  [ getMin (m ^. _1) + getMax (m ^. _2)
-  | ys <- tails (map (\x -> (Min x, Max x, Sum x)) xs)
-  , m <- scanl (<>) mempty ys
-  , getSum (m ^. _3) == total ]
+part1 = maybe 0 last . find filt . transpose . take 26 . tails where
+  filt (reverse -> x:xs) = all ((/= x) . sum) (replicateM 2 xs)
+part2 xs@(part1 -> n) = maybe 0 ((+) <$> minimum <*> maximum)
+  . find ((== n) . sum) . (tails <=< inits) $ takeWhile (/= n) xs
 
 main = do
   input <- readFile (replaceExtension __FILE__ ".in")
-  print . part2 . takeTilFail . map read $ lines input
+  print . part2 $ readOnlyNums input
 
