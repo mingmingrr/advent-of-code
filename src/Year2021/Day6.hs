235d2f236793242e1dbeb5c36729f5259562a45c
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-06 00:45:13 -0500

    2021.6: use lenses

--- src/Year2021/Day6.hs
+++ src/Year2021/Day6.hs
@@ -3,6 +3,7 @@
 module Year2021.Day6 where
 
 import Util
+import Control.Lens
 import System.FilePath
 
 part1, part2 :: Int
@@ -10,6 +11,6 @@ part1 = 80
 part2 = 256
 
 main = readFile (replaceExtension __FILE__ ".in") >>= print . sum
-  . (!! part2) . iterate (\(x:xs) -> zipWith (+) [0,0,0,0,0,0,x,0,x] (xs++[0]))
+  . (!! part2) . iterate (\(x:xs) -> ix 6 +~ x $ xs++[x])
   . (\m -> map (m !??) [0..8]) . counter . readOnlyNums
 
