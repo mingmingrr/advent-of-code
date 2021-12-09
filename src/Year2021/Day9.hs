6a57ee5ffe06f4fe79b5483c517cf52d7959c867
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-09 00:55:17 -0500

    2021.9: simplify

--- src/Year2021/Day9.hs
+++ src/Year2021/Day9.hs
@@ -6,22 +6,21 @@ import Util
 import Linear.V2
 import Data.List
 import Data.Ord
+import Data.Char
 import System.FilePath
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 part1, part2 :: Map (V2 Int) Char -> Int
-part1 canyon = sum [fromEnum v + 1
+part1 canyon = sum [digitToInt v + 1
   | (k, v) <- Map.toList canyon
   , and [maybe True (> v) (canyon Map.!? (k + d)) | d <- adjacent] ]
-part2 canyon = product . take 3 . sortOn Down $ run (Map.keys canyon) where
-  run [] = []
-  run (n:ns)
-    | canyon Map.! n == '9' = run ns
-    | otherwise = length visit : run (ns \\ visit)
-    where visit = map (head . snd) (dijkstra neigh n)
-          neigh p = [(1, n) | n <- map (+p) adjacent,
-            maybe False (/= '9') (canyon Map.!? n)]
+part2 canyon = product . take 3 . sortOn Down
+  . run . Map.keys $ Map.filter (/= '9') canyon
+  where run [] = []
+        run (n:ns) = length visit : run (ns \\ visit)
+          where visit = map (head . snd) (dijkstra neigh n)
+                neigh p = [(1, n) | n <- map (+p) adjacent, n `elem` ns]
 
 main = readFile (replaceExtension __FILE__ ".in") >>=
   print . part2 . Map.fromList . labelGrid . lines
