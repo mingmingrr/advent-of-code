57f69c00f5e4973e714dccc9f8cf267999417a01
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-10 02:47:04 -0500

    2021.10: use Megaarsec

--- src/Year2021/Day10.hs
+++ src/Year2021/Day10.hs
@@ -1,22 +1,20 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Year2021.Day10 where
 
-import Util
 import Safe
-import Data.Maybe
-import Data.Either
+import Text.Megaparsec
+import Text.Megaparsec.Char
 import Data.List
-import System.FilePath
+import Data.Either
+import Data.Ord
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Set as Set
 import qualified Data.Bimap as Bimap
-
-pairs :: String -> String -> Either Char String
-pairs ns [] = Right ns
-pairs [] (x:xs) = pairs [x] xs
-pairs (n:ns) (x:xs) = case (== n) <$> Bimap.lookupR x brackets of
-  Nothing -> pairs (x:n:ns) xs
-  Just True -> pairs ns xs
-  _ -> Left x
+import Control.Monad
+import System.FilePath
 
 part1, part2 :: [Either Char String] -> Int
 part1 = sum . map (`lookupJust` table) . lefts
@@ -25,6 +23,22 @@ part2 = fst . head . filter (uncurry (==)) . (zip <*> reverse) . sort . map scor
   where table = [('(', 1), ('[', 2), ('{', 3), ('<', 4)]
         score = foldl (\s x -> s * 5 + x) 0 . map (`lookupJust` table)
 
-main = readFile (replaceExtension __FILE__ ".in") >>= 
-  print . part2 . map (pairs []) . lines
+parser :: Parsec (Int, Char) String ()
+parser = void . many . choice . map match $ words "{} [] () <>" where
+  match [x, y] = do
+    o <- getOffset
+    single x
+    observing (parser *> single y) >>= \case
+      Left err -> registerFancyFailure (Set.singleton (ErrorCustom (o, x))) *> parseError err
+      Right _ -> return ()
+
+foldErrors :: Either (ParseErrorBundle String (Int, Char)) a -> Either Char String
+foldErrors (Left ParseErrorBundle{..}) = case token of
+  Just (Tokens (x NE.:| _)) -> Left x
+  Just EndOfInput -> Right . map snd $ sortOn Down
+    [e | FancyError _ xs <- expected, ErrorCustom e <- Set.toList xs]
+  where TrivialError _ token _ NE.:| expected = bundleErrors
+
+main = readFile (replaceExtension __FILE__ ".in") >>=
+  print . part2 . map (foldErrors . parse (parser <* eof) "") . lines
 
