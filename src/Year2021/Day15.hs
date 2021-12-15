fb08c6dae8045a72595bb9b38a393b49145cd4f9
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-12-15 01:29:59 -0500

    2021.15: use matrix

--- src/Year2021/Day15.hs
+++ src/Year2021/Day15.hs
@@ -2,25 +2,23 @@
 
 module Year2021.Day15 where
 
+import Numeric.LinearAlgebra
 import Util
 import Safe
 import Linear.V2
 import System.FilePath
-import Control.Monad
-import Data.Char
-import qualified Data.Map as Map
+import Data.Bifunctor
 
 part1, part2 :: Int
 part1 = 1
 part2 = 5
 
 main = readFile (replaceExtension __FILE__ ".in") >>= \input ->
-  let grid = Map.map digitToInt . Map.fromList . labelGrid $ lines input
-      high = snd (bounds (Map.keys grid)) + 1
-      grid' = Map.unions
-        [ Map.mapKeysMonotonic (+ offset * (fst (Map.findMax grid) + 1))
-        $ Map.map (succ . flip mod 9 . pred . (+ sum offset)) grid
-        | offset <- join (liftM2 V2) [0..part2-1] ]
-      neigh n = [(v, d) | d <- (+ n) <$> adjacent, Just v <- [grid' Map.!? d]]
-   in print . fst . findJust ((== fst (Map.findMax grid')) . head . snd) $ dijkstra neigh 0
+  let grid = fromLists . map (map (read . pure)) $ lines input :: Matrix Z
+      grid' = cmod 9 (fromBlocks [[grid + scalar (r + c) | c <- [0..4]] | r <- [0..4]] - 1) + 1
+      neigh (a, b) = [ (grid' ! x ! y, (x, y))
+        | p@(V2 x y) <- (+ V2 a b) <$> adjacent
+        , inBounds 0 (uncurry V2 (size grid') - 1) p ]
+   in print . fst . findJust ((== size grid') . bimap succ succ . head . snd)
+        $ dijkstra neigh (0, 0)
 
