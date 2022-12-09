55cef2090cdc9b6a17b40d6f0781804d8478c8c6
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-09 00:37:44 -0500

    2022.9: complete

---
+++ src/Year2022/Day9.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE CPP #-}
+
+module Year2022.Day9 where
+
+import Util
+import Safe
+import Linear.V2
+import System.FilePath
+import Data.Map ((!))
+
+snek :: V2 Int -> V2 Int -> V2 Int
+snek x y | all (< 2) (abs (x - y)) = y
+snek x y = findJust (all (< 2) . abs . subtract x) . map (+ y) $
+  if 0 `elem` (x - y) then adjacent else diagonal
+
+part1, part2 :: Int
+part1 = 2
+part2 = 10
+
+main = readFile (replaceExtension __FILE__ ".in")
+  >>= print . length . nubSet . map last
+    . scanl (\(n:ns) dir -> scanl snek (n + directions ! dir) ns) (replicate part2 0)
+    . concatMap (uncurry' (flip (replicate . read)) . words) . lines
+
