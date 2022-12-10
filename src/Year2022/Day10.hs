2a01e4c9794e3da077484910454764d8bb825b37
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-10 02:41:12 -0500

    2022.10: use readMaybe

--- src/Year2022/Day10.hs
+++ src/Year2022/Day10.hs
@@ -2,13 +2,9 @@
 
 module Year2022.Day10 where
 
+import Text.Read
 import Data.List.Split
 import System.FilePath
-
-run :: Int -> [[String]] -> [Int]
-run reg (["addx", num]:xs) = reg : reg : run (reg + read num) xs
-run reg (["noop"]:xs) = reg : run reg xs
-run reg [] = repeat reg
 
 part1, part2 :: [Int] -> String
 part1 = show . sum . map head . take 6
@@ -18,5 +14,5 @@ part2 = unlines . take 6 . chunksOf 40
   . zipWith subtract [-1..]
 
 main = readFile (replaceExtension __FILE__ ".in") >>=
-  putStrLn . part1 . run 1 . map words . lines
+  putStrLn . part1 . scanl (+) 1 . map (maybe 0 id . readMaybe) . words
 
