3ce6901333d8adb0c46772a58b69bbe23ffe6d9c
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-11 03:21:44 -0500

    2022.11: use monads

--- src/Year2022/Day11.hs
+++ src/Year2022/Day11.hs
@@ -1,36 +1,37 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Year2022.Day11 where
 
 import Util
 import Data.List
+import Data.List.Split
 import Data.Ord
-import Control.Monad
+import Control.Monad.Writer
+import Control.Monad.State
 import Control.Lens
-
 import System.FilePath
 
 type Monkey = ([Int], Int -> Int, [Int])
 
 parse :: [String] -> Monkey
-parse (_:items:update:test) = (readOnlyNums items, update', readOnlyNums =<< test)
-  where update' = case reverse (words update) of
-          ("old":fn:_) -> maybe undefined join (lookup fn funcs)
-          (num:fn:_) -> maybe undefined ($ read num) (lookup fn funcs)
-        funcs = [("+", (+)), ("*", (*))]
+parse (index:items:update:check) = (readOnlyNums items, update', check') where
+  update' = case reverse (words update) of
+    ("old":fn:_) -> maybe undefined join (lookup fn funcs)
+    (num:fn:_) -> maybe undefined ($ read num) (lookup fn funcs)
+  funcs = [("+", (+)), ("*", (*))]
+  check' = _tail . each %~ subtract (head (readOnlyNums index)) $ readOnlyNums =<< check
 
-run :: (Int -> Int) -> Int -> [Monkey] -> [Monkey]
-run modulus index monkeys = ix index . _1 .~ [] $ foldr throw monkeys items
-  where (items, modworry, [check, target1, target2]) = monkeys !! index
-        throw item = ix @[Monkey] towards . _1 %~ (item':)
-          where item' = modulus (modworry item)
-                towards = if mod item' check == 0 then target1 else target2
-
-runRound :: (Int -> [Monkey] -> [Monkey]) -> [Monkey] -> ([Int], [Monkey])
-runRound run monkeys = (thrown, last runs) where
-  runs = scanl (flip run) monkeys [0 .. length monkeys - 1]
-  thrown = zipWith (\n xs -> (xs !! n) ^. _1 . to length) [0..] (init runs)
+run :: (Int -> Int) -> StateT [Monkey] (Writer ([Int] -> [Int])) ()
+run modulus = do
+  indexed <- gets (flip mod . length)
+  (items, modworry, check') <- gets head
+  let [check, target1, target2] = check'
+  tell (length items :)
+  forM_ items $ \item -> do
+    let item' = modulus (modworry item)
+        towards = if mod item' check == 0 then target1 else target2
+    ix (indexed towards) . _1 %= (item':)
+  modify ((++) <$> tail <*> pure . set _1 [] . head)
 
 part1, part2 :: (Int, Int -> Int -> Int)
 part1 = (20, const (`div` 3))
@@ -38,6 +39,8 @@ part2 = (10000, flip mod)
 
 main = do
   monkeys <- map parse . paragraphs <$> readFile (replaceExtension __FILE__ ".in")
-  let (rounds, modulus) = part1
-  print . product . take 2 . sortOn Down . foldl1 (zipWith (+)) . take rounds
-    $ unfoldr (Just . runRound (run (modulus (product (map (^. _3 . to head) monkeys))))) monkeys
+  let (rounds, modulus) = part2
+  print . product . take 2 . sortOn Down . foldl1 (zipWith (+))
+    . take rounds . chunksOf (length monkeys) . ($ [])
+    . execWriter . flip runStateT monkeys . forever
+    . run . modulus . product $ map (^. _3 . to head) monkeys
