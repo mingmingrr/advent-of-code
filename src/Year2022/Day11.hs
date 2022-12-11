c2eae95e5c0255b600e010d1a8d7dda960b8ec40
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-11 01:45:35 -0500

    2022.11: complete

---
+++ src/Year2022/Day11.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Year2022.Day11 where
+
+import Util
+import Data.List
+import Data.Ord
+import Control.Monad
+import Control.Lens
+
+import System.FilePath
+
+type Monkey = ([Int], Int -> Int, [Int])
+
+parse :: [String] -> Monkey
+parse (_:items:update:test) = (readOnlyNums items, update', readOnlyNums =<< test)
+  where update' = case reverse (words update) of
+          ("old":fn:_) -> maybe undefined join (lookup fn funcs)
+          (num:fn:_) -> maybe undefined ($ read num) (lookup fn funcs)
+        funcs = [("+", (+)), ("*", (*))]
+
+run :: Int -> Int -> Int -> [Monkey] -> [Monkey]
+run divider modulus index monkeys = ix index . _1 .~ [] $ foldr throw monkeys items
+  where (items, modworry, [check, target1, target2]) = monkeys !! index
+        throw item = ix @[Monkey] towards . _1 %~ (item':)
+          where item' = modworry item `div` divider `mod` modulus
+                towards = if mod item' check == 0 then target1 else target2
+
+runRound :: (Int -> [Monkey] -> [Monkey]) -> [Monkey] -> ([Int], [Monkey])
+runRound run monkeys = (thrown, last runs) where
+  runs = scanl (flip run) monkeys [0 .. length monkeys - 1]
+  thrown = zipWith (\n xs -> (xs !! n) ^. _1 . to length) [0..] (init runs)
+
+part1, part2 :: (Int, Int)
+part1 = (20, 3)
+part2 = (10000, 1)
+
+main = do
+  monkeys <- map parse . paragraphs <$> readFile (replaceExtension __FILE__ ".in")
+  let modulus = product (map (^. _3 . to head) monkeys)
+      (rounds, divider) = part2
+  print . product . take 2 . sortOn Down . foldl1 (zipWith (+)) . take rounds
+    $ unfoldr (Just . runRound (run divider modulus)) monkeys
