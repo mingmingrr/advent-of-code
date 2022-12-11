e23ee4d9422de82c5fdca4b725d8c402cdeee3e8
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-11 03:13:42 -0500

    2022.11: fix modulus

--- src/Year2022/Day11.hs
+++ src/Year2022/Day11.hs
@@ -20,11 +20,11 @@ parse (_:items:update:test) = (readOnlyNums items, update', readOnlyNums =<< tes
           (num:fn:_) -> maybe undefined ($ read num) (lookup fn funcs)
         funcs = [("+", (+)), ("*", (*))]
 
-run :: Int -> Int -> Int -> [Monkey] -> [Monkey]
-run divider modulus index monkeys = ix index . _1 .~ [] $ foldr throw monkeys items
+run :: (Int -> Int) -> Int -> [Monkey] -> [Monkey]
+run modulus index monkeys = ix index . _1 .~ [] $ foldr throw monkeys items
   where (items, modworry, [check, target1, target2]) = monkeys !! index
         throw item = ix @[Monkey] towards . _1 %~ (item':)
-          where item' = modworry item `div` divider `mod` modulus
+          where item' = modulus (modworry item)
                 towards = if mod item' check == 0 then target1 else target2
 
 runRound :: (Int -> [Monkey] -> [Monkey]) -> [Monkey] -> ([Int], [Monkey])
@@ -32,13 +32,12 @@ runRound run monkeys = (thrown, last runs) where
   runs = scanl (flip run) monkeys [0 .. length monkeys - 1]
   thrown = zipWith (\n xs -> (xs !! n) ^. _1 . to length) [0..] (init runs)
 
-part1, part2 :: (Int, Int)
-part1 = (20, 3)
-part2 = (10000, 1)
+part1, part2 :: (Int, Int -> Int -> Int)
+part1 = (20, const (`div` 3))
+part2 = (10000, flip mod)
 
 main = do
   monkeys <- map parse . paragraphs <$> readFile (replaceExtension __FILE__ ".in")
-  let modulus = product (map (^. _3 . to head) monkeys)
-      (rounds, divider) = part2
+  let (rounds, modulus) = part1
   print . product . take 2 . sortOn Down . foldl1 (zipWith (+)) . take rounds
-    $ unfoldr (Just . runRound (run divider modulus)) monkeys
+    $ unfoldr (Just . runRound (run (modulus (product (map (^. _3 . to head) monkeys))))) monkeys
