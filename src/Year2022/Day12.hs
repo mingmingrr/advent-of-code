7408f34f7b83279bd6f291545ddea9abd22207ec
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-12 01:14:25 -0500

    2022.12: reverse search

--- src/Year2022/Day12.hs
+++ src/Year2022/Day12.hs
@@ -10,7 +10,7 @@ import qualified Data.Map as Map
 
 neigh :: Map (V2 Int) Char -> V2 Int -> [(Int, V2 Int)]
 neigh grid p = map ((,) 1) (neighbors adjacent
-  [ maybe False ((<= elev (grid Map.! p) + 1) . elev) . flip Map.lookup grid ] p)
+  [ maybe False ((>= elev (grid Map.! p) - 1) . elev) . flip Map.lookup grid ] p)
   where elev x = fromEnum . maybe x id $ lookup x [('S', 'a'), ('E', 'z')]
 
 part1, part2 :: [Char]
@@ -18,6 +18,6 @@ part1 = "S"
 part2 = "Sa"
 
 main = Map.fromList . labelGrid . lines <$> readFile (replaceExtension __FILE__ ".in")
-  >>= \grid -> print . minimum . map fst . filter ((== 'E') . (grid Map.!) . head . snd)
-             $ dijkstra (neigh grid) =<< Map.keys (Map.filter (`elem` part2) grid)
+  >>= \grid -> print . minimum . map fst . filter ((`elem` part2) . (grid Map.!) . head . snd)
+             $ dijkstra (neigh grid) =<< Map.keys (Map.filter (== 'E') grid)
 
