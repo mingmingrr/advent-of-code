ee53ca56621d62e769d0bd7498152c8986e18a63
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2022-12-12 00:29:11 -0500

    2022.12: complete

---
+++ src/Year2022/Day12.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE CPP #-}
+
+module Year2022.Day12 where
+
+import Util
+import Linear.V2
+import System.FilePath
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+neigh :: Map (V2 Int) Char -> V2 Int -> [(Int, V2 Int)]
+neigh grid p = map ((,) 1) (neighbors adjacent
+  [ maybe False ((<= elev (grid Map.! p) + 1) . elev) . flip Map.lookup grid ] p)
+  where elev x = fromEnum . maybe x id $ lookup x [('S', 'a'), ('E', 'z')]
+
+part1, part2 :: [Char]
+part1 = "S"
+part2 = "Sa"
+
+main = Map.fromList . labelGrid . lines <$> readFile (replaceExtension __FILE__ ".in")
+  >>= \grid -> print . minimum . map fst . filter ((== 'E') . (grid Map.!) . head . snd)
+             $ dijkstra (neigh grid) =<< Map.keys (Map.filter (`elem` part2) grid)
+
