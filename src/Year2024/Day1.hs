f92725e0f27c5aff337d067291fb451e14252d41
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2025-03-05 06:09:36 -0500

    2024.1b: complete

--- src/Year2024/Day1.hs
+++ src/Year2024/Day1.hs
@@ -3,11 +3,14 @@
 module Year2024.Day1 where
 
 import Util
-import Safe
+import qualified Data.Map as Map
 import Data.List
 import System.FilePath
 
+part1, part2 :: [[Int]] -> [Int]
+part1 = uncurry' (zipWith ((abs .) . (-)))
+part2 = Map.elems . uncurry' (Map.intersectionWithKey (((*) .) . (*))) . map counter
+
 main = readFile (replaceExtension __FILE__ ".in") >>= print
-  . sum . uncurry' (zipWith ((abs .) . (-)))
-  . map sort . transpose . map readOnlyNums . lines
+  . sum . part2 . map sort . transpose . map readOnlyNums . lines
 
